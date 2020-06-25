fff25fbac34f6155308a8c291ec7c76193c32b5b
vvbaliga <58920999+vvbaliga@users.noreply.github.com>
2020-06-24 23:03:36 -0400

    Create class1.py

---
+++ Google-IT-Automation/PythonCrashCourse/Week5/class1.py
@@ -0,0 +1,15 @@
+class Flower:
+  color = 'unknown'
+
+
+rose = Flower()
+rose.color = "red"
+
+violet = Flower()
+violet.color = "blue"
+
+this_pun_is_for_you = "who is your daddy and what does he do"
+
+print("Roses are {},".format(rose.color))
+print("violets are {},".format(violet.color))
+print(this_pun_is_for_you)
